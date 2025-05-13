9dd816510825c22268f6b99bb620128970dace5d
Maxim Varshavsky <code4rizz@gmail.com>
2025-05-12 19:56:38 -0700

    Initial commit

---
+++ svelte-project-1/my-svelte-app/src/App.svelte
@@ -0,0 +1,43 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  <div>
+    <img src={viteLogo} class="logo" alt="Vite Logo" />
+    <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+  </div>
+  <h1>Svelte Counter Game</h1>
+
+  <div class="card">
+    <Counter />
+  </div>
+
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer" style="text-decoration: underline; color: inherit;">SvelteKit</a>, the official Svelte app framework powered by Vite!
+  </p>
+
+  <p class="read-the-docs">
+    Click on the Vite and Svelte logos to learn more
+  </p>
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #555;
+  }
+</style>
---
+++ svelte-project-1/my-svelte-app/src/lib/Counter.svelte
@@ -0,0 +1,68 @@
+<script>
+  let increaseAmountClicks = $state(0);
+  let incrementAmount = $state(1);
+  let increaseAutoAmount = $state(5);
+  let count = $state(0);
+  let clickCost = $state(15);
+  let autoCost = $state(25);
+  let autoCountStarted = $state(false);
+
+  const increment = () => {
+    count += incrementAmount;
+  };
+
+  const boostIncrement = () => {
+    if (count >= clickCost) {
+      count -= clickCost
+      increaseAmountClicks += 1;
+      if (increaseAmountClicks === 1) {
+        incrementAmount += 9;
+      }
+      else {
+        incrementAmount += 10;
+      }
+      clickCost *= 5;
+    };
+  };
+  const startAutoCount = () => {
+    if (count >= autoCost) {
+      setInterval(() => {
+        count += increaseAutoAmount;
+      }, 1000);
+      autoCountStarted = true;
+    };
+  };
+
+  const upgradeAutoCount = () => {
+    if (count >= autoCost) {
+      count -= autoCost;
+      increaseAutoAmount += 5;
+      autoCost *= 7;
+    }
+  };
+</script>
+
+<button onclick={increment}>
+  Count is {count}
+</button>
+<br>
+<button onclick={boostIncrement}>
+  Increase Increment (Now: {incrementAmount})
+</button>
+<br>
+<h4>Cost of next click upgrade: {clickCost}</h4>
+<br><br>
+{#if incrementAmount >= 40 && autoCountStarted === false}
+  <button onclick={startAutoCount}>
+    Start Auto Count +{increaseAutoAmount}/sec (costs 40)
+  </button>
+  <br>
+{/if}
+{#if autoCountStarted}
+  <button onclick={upgradeAutoCount}>
+    Upgrade Auto Count (Now: +{increaseAutoAmount}/sec)
+  </button>
+  <br>
+  <h4>Cost of next Auto Upgrade: {autoCost}</h4>
+  <br><br>
+{/if}
